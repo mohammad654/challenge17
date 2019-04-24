842241a504a4d861fe695b3275fdadb82bf24c29
mohammad654 <mohammad.ali.shikhi.55@gmail.com>
2019-04-24 11:23:13 +0200

    Opdrachten gemaakt

---
+++ challange1/myphp.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "<br>";
+$name='str to lower';
+
+if (strtolower($name)==='str to lower'){
+    echo '<em>Yes str to lower  =  str to lower </em>'."<br>";
+}else {
+    echo 'No str to lower # str to lower ';
+}
+ //--------________--------_________-------------_____________--------__________________-------_.
+
+echo "<br>";
+print "<strong>Print </strong>"."<br>";
+
+$number=4;
+while($number<=10){
+    echo '<br>'.$number. ' Yes';
+    $number++;
+}
